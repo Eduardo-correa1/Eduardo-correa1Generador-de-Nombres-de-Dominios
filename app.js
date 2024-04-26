b71f593f72ece58e54e8daaf92d90f9facebd2c9
Eduardo-correa1 <159133054+Eduardo-correa1@users.noreply.github.com>
2024-04-26 19:17:13 +0000

    formapyeach

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log("Cualquier string");
+let pron = ["the", "our"];
+let adj = ["fantastic", "drunk","sleeping"];
+let noun = ["fish", "mother", "alien", "horse", "squid"];
+let dom = [".com", ".net"];
+//bucle for
+const generadorDominiofor = () =>{
+    for(let p = 0; p < pron.length; p++){
+        for(let a = 0; a < adj.length; a++){
+            for(let n = 0; n < noun.length; n++){
+                for(let d = 0; d < dom.length; d++){
+                let dominio = pron[p] + adj[a] + noun[n] + dom[d];
+            console.log(dominio)
+            }
+        }
+        }
+    }
+}
+// bucle map
+ const generadorDominiomap = () =>{
+pron.map((p) =>{
+    adj.map((a)=>{
+        noun.map((n)=>{
+            dom.map((d)=>{
+                let dominio = p + a + n + d;
+            console.log(dominio);
+            });
+        });
+    });
+});
+};
+// foreach
+const generadorDominiofe = () =>{
+    pron.forEach((p) =>{
+        adj.forEach((a)=>{
+            noun.forEach((n)=>{
+                dom.forEach((d)=>{
+                    let dominio = p + a + n + d;
+                console.log(dominio);
+                });
+            });
+        });
+    });
+    };
+generadorDominiofe();
+generadorDominiomap();
+generadorDominiofor();
